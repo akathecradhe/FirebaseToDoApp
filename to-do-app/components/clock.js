5784e0d2e61e7e44100735b3f7c83ba14ad243af
clive <tsunguc@cardiff.ac.uk>
2022-05-01 17:49:47 +0100

    build: install tailwind.css

--- to-do-app/components/clock.js
+++ to-do-app/components/clock.js
@@ -1,28 +1,18 @@
 import { useSelector } from 'react-redux'
+import styled from 'styled-components';
 
 const formatTime = (time) => {
   return new Date(time).toJSON().slice(11, 19)
 }
 
+
 const Clock = () => {
   const lastUpdate = useSelector((state) => state.timer.lastUpdate)
   const light = useSelector((state) => state.timer.light)
   return (
-    <div className={light ? 'light' : ''}>
+    <div >
       {formatTime(lastUpdate)}
-      <style jsx>{`
-        div {
-          padding: 15px;
-          display: inline-block;
-          color: #82fa58;
-          font: 50px menlo, monaco, monospace;
-          background-color: #000;
-        }
-
-        .light {
-          background-color: #999;
-        }
-      `}</style>
+    
     </div>
   )
 }
