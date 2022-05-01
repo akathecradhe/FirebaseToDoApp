915884f037ef3ec2eb4c0a8eb9eefd7e88e5e9ec
clive <tsunguc@cardiff.ac.uk>
2022-05-01 23:38:59 +0100

    [2] feat: configured redux to remove an item

--- to-do-app/actions.js
+++ to-do-app/actions.js
@@ -2,5 +2,6 @@ import * as types from './types'
 // Adds Todo
 export const AddToDo = () => ({type: 'todos/ADD-TO-DO', payload: todotext})
 
+export const RemoveToDo = () => ({type: 'todos/REMOVE-TO-DO', payload: todotext})
 
 
--- to-do-app/reducers.js
+++ to-do-app/reducers.js
@@ -22,6 +22,10 @@ const toDoReducer = (state = initialTodoState, action) => {
           {id: nextTodoId(state.todos),
           text: action.payload,}]
       }
+    case 'todos/REMOVE-TO-DO':
+      return {
+        todos: [...state.todos.filter( todo => todo.id !== action.payload )]
+        }  
     default:
       return state
   }
