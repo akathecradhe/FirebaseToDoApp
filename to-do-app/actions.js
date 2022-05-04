d162eee07747f37519b332659947e97db3a218a4
clive <tsunguc@cardiff.ac.uk>
2022-05-04 08:21:03 +0100

    dev branch git init

--- to-do-app/actions.js
+++ to-do-app/actions.js
@@ -2,6 +2,7 @@ import * as types from './types'
 // Adds Todo
 export const AddToDo = () => ({type: 'todos/ADD-TO-DO', payload: todotext})
 
+
 export const RemoveToDo = (id,text) => ({type: 'todos/REMOVE-TO-DO', payload: {id,text} })
 
 
