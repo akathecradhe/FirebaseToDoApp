78aa8e2337b0853597d1e8c70ff594730d290b02
clive <tsunguc@cardiff.ac.uk>
2022-05-04 10:11:09 +0100

    [4] build: firebase config

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -6,7 +6,6 @@ function App() {
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">
           To Do Application
-
         </h1>
         <Layout/>
       </header>
